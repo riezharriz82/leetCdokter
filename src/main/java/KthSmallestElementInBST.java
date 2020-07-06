14d8ecc316a093f7fe8067078333bfdfdce0f722
snehasishiiita <rit2011091@iiita.ac.in>
2020-07-06 16:47:47 +0530

    Solve medium KthSmallestElementInBST using inorder traversal

---
+++ src/main/java/KthSmallestElementInBST.java
@@ -0,0 +1,44 @@
+/**
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * <p>
+ * Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: root = [3,1,4,null,2], k = 1
+ * <pre>
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * </pre>
+ * Output: 1
+ */
+public class KthSmallestElementInBST {
+    int count;
+
+    public int kthSmallest(TreeNode root, int k) {
+        count = k;
+        return inorderTraversal(root);
+    }
+
+    private int inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+        int leftRes = inorderTraversal(root.left);
+        if (leftRes != Integer.MAX_VALUE) {
+            count--;
+            if (count == 0) {
+                return root.val;
+            }
+            return inorderTraversal(root.right);
+
+        } else {
+            return leftRes;
+        }
+    }
+}
