29becd6e59a261f8b1567995750f86fab40bbc95
snehasishiiita <rit2011091@iiita.ac.in>
2020-07-18 02:23:11 +0530

    Update documentation in DeleteNodeInLinkedListWithoutHeadPointer

--- src/main/java/DeleteNodeInLinkedListWithoutHeadPointer.java
+++ src/main/java/DeleteNodeInLinkedListWithoutHeadPointer.java
@@ -1,5 +1,11 @@
 /**
  * https://leetcode.com/problems/delete-node-in-a-linked-list/
+ * <p>
+ * Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+ * <p>
+ * Input: head = [4,5,1,9], node = 5
+ * Output: [4,1,9]
+ * Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
  */
 public class DeleteNodeInLinkedListWithoutHeadPointer {
     public void deleteNode(ListNode node) {
