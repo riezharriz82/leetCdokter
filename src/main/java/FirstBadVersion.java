9ef05ed34dc7a5ac88674485eddd9553ca5088ab
snehasishiiita <rit2011091@iiita.ac.in>
2020-08-03 01:29:55 +0530

    Solve easy find first bad commit version by binary search

---
+++ src/main/java/FirstBadVersion.java
@@ -0,0 +1,43 @@
+/**
+ * https://leetcode.com/problems/first-bad-version/
+ * <p>
+ * You are a product manager and currently leading a team to develop a new product.
+ * Unfortunately, the latest version of your product fails the quality check.
+ * Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+ * <p>
+ * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+ * <p>
+ * You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version.
+ * You should minimize the number of calls to the API.
+ * <p>
+ * Given n = 5, and version = 4 is the first bad version.
+ * <p>
+ * call isBadVersion(3) -> false
+ * call isBadVersion(5) -> true
+ * call isBadVersion(4) -> true
+ * <p>
+ * Then 4 is the first bad version.
+ */
+public class FirstBadVersion {
+    //Standard binary search
+    public int firstBadVersion(int n) {
+        return binarySearch(1, n);
+    }
+
+    private int binarySearch(int low, int high) {
+        if (low >= high) {
+            return low;
+        }
+        int mid = low + (high - low) / 2;
+        if (isBadVersion(mid)) {
+            return binarySearch(low, mid);
+        } else {
+            return binarySearch(mid + 1, high);
+        }
+    }
+
+    //placeholder function
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
+}
