32323fbd09fe279f4a837a62346ddb8f9a695cbf
snehasishiiita <rit2011091@iiita.ac.in>
2020-08-02 17:42:46 +0530

    Update problem statement in ZigZagTraversal

--- src/main/java/ZigZagTraversal.java
+++ src/main/java/ZigZagTraversal.java
@@ -4,6 +4,27 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+ * <p>
+ * Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
+ * <p>
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7]
+ * <pre>
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * </pre>
+ * return its zigzag level order traversal as:
+ * [
+ * [3],
+ * [20,9],
+ * [15,7]
+ * ]
+ */
 public class ZigZagTraversal {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> res = new ArrayList<>();
