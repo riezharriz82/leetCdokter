b0f2f883ede01246e8ed36580d369b42dc1e14da
snehasishiiita <rit2011091@iiita.ac.in>
2020-08-21 23:19:46 +0530

    Update medium find K closest point to origin using priority queue.
    Learnings -- Whenever asked to find top k elements, think about priority queue. Quick select can also be used.

--- src/main/java/KClosestPointToOrigin.java
+++ src/main/java/KClosestPointToOrigin.java
@@ -1,42 +1,47 @@
-import javafx.util.Pair;
-
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
  * https://leetcode.com/problems/k-closest-points-to-origin/
+ * <p>
+ * We have a list of points on the plane.  Find the K closest points to the origin (0, 0).
+ * <p>
+ * (Here, the distance between two points on a plane is the Euclidean distance.)
+ * <p>
+ * You may return the answer in any order.  The answer is guaranteed to be unique (except for the order that it is in.)
+ * <p>
+ * Input: points = [[1,3],[-2,2]], K = 1
+ * Output: [[-2,2]]
+ * Explanation:
+ * The distance between (1, 3) and the origin is sqrt(10).
+ * The distance between (-2, 2) and the origin is sqrt(8).
+ * Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
+ * We only want the closest K = 1 points from the origin, so the answer is just [[-2,2]].
  */
 public class KClosestPointToOrigin {
     public int[][] kClosestUsingPriorityQueue(int[][] points, int K) {
-        long dist;
-        PriorityQueue<Pair<Long, Pair<Integer, Integer>>> priorityQueue = new PriorityQueue<>(K, new Comparator<Pair<Long, Pair<Integer, Integer>>>() {
-            @Override
-            public int compare(Pair<Long, Pair<Integer, Integer>> o1, Pair<Long, Pair<Integer, Integer>> o2) {
-                return (int) (o2.getKey() - o1.getKey());
-            }
-        });
-        for (int i = 0; i < points.length; i++) {
-            int x = points[i][0];
-            int y = points[i][1];
-            dist = x * x + y * y;
-            if (priorityQueue.isEmpty())
-                priorityQueue.add(new Pair<>(dist, new Pair<>(x, y)));
-            else if (priorityQueue.size() == K) { //queue is full, need to remove element if we have found a smaller distance
-                if (priorityQueue.peek().getKey() > dist) { //we have found a smaller distance
-                    priorityQueue.poll();
-                    priorityQueue.add(new Pair<>(dist, new Pair<>(x, y)));
-                }
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingInt(o -> -o.distance)); //notice the negative sign, indicating max heap
+        for (int[] point : points) {
+            int x = point[0];
+            int y = point[1];
+            int distance = x * x + y * y;
+            if (pq.size() < K) {
+                pq.add(new Point(x, y, distance));
             } else {
-                priorityQueue.add(new Pair<>(dist, new Pair<>(x, y)));
+                if (pq.peek().distance > distance) {
+                    //need only k closest points, found a closer point, hence removing the farthest point
+                    pq.remove();
+                    pq.add(new Point(x, y, distance));
+                }
             }
         }
         int[][] result = new int[K][2];
-        int tmp = 0;
-        for (Pair<Long, Pair<Integer, Integer>> k : priorityQueue) {
-            result[tmp][0] = k.getValue().getKey();
-            result[tmp][1] = k.getValue().getValue();
-            tmp++;
+        int index = K - 1;
+        while (index >= 0) { //the problem accepts answers in any order, still returning in correct order
+            Point farthestPoint = pq.remove();
+            result[index][0] = farthestPoint.x;
+            result[index--][1] = farthestPoint.y;
         }
         return result;
     }
@@ -53,9 +58,21 @@ public class KClosestPointToOrigin {
             } else {
                 r = mid - 1;
             }
-
         }
+        //problem is asking to return them in any order
         return Arrays.copyOfRange(points, 0, K);
+    }
+
+    private static class Point {
+        int x;
+        int y;
+        int distance;
+
+        public Point(int x, int y, int distance) {
+            this.x = x;
+            this.y = y;
+            this.distance = distance;
+        }
     }
 
     private int partition(int[][] points, int l, int r) {
@@ -69,7 +86,6 @@ public class KClosestPointToOrigin {
         }
         swap(points, i, r);
         return i;
-
     }
 
     private void swap(int[][] points, int i, int j) {
