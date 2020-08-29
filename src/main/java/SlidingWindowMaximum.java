2cb74cdaad02131b7d903ceada9960016df21d2b
snehasishiiita <rit2011091@iiita.ac.in>
2020-08-29 21:55:25 +0530

    Solve hard find the maximum number for all sliding windows in linear time using deque. Related to find max length of subarray whose diff of min and max element lies within a limit
    Learning -- Sliding window implementation using deque.

---
+++ src/main/java/SlidingWindowMaximum.java
@@ -0,0 +1,58 @@
+import java.util.ArrayDeque;
+
+/**
+ * https://leetcode.com/problems/sliding-window-maximum/
+ * <p>
+ * Given an array nums, there is a sliding window of size k which is moving from the very left of the array to the very right.
+ * You can only see the k numbers in the window. Each time the sliding window moves right by one position. Return the max sliding window.
+ * <p>
+ * Follow up:
+ * Could you solve it in linear time?
+ * <p>
+ * Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+ * Output: [3,3,5,5,6,7]
+ * Explanation:
+ * <pre>
+ * Window position                Max
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ *  </pre>
+ */
+public class SlidingWindowMaximum {
+    /**
+     * See {@link LongestContiguousSubarrayWithValidDiff} for similar sliding window problem using deque
+     * Keep track of the useful numbers in the queue i.e. pop all numbers smaller than current number from the queue as they can't be maximum
+     * till the window ending at current index.
+     * However if you see a number smaller than the last element in queue, you need to add it, as it's possible that it can become the maximum
+     * once the window slides.
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        ArrayDeque<Integer> queue = new ArrayDeque<>(); //store the index, not the number to easily track whether that index has fallen out of window
+        for (int i = 0; i < k; i++) { //for the first k elements
+            while (!queue.isEmpty() && nums[queue.peekLast()] < nums[i]) {
+                //remove number smaller than current element
+                queue.pollLast();
+            }
+            queue.add(i);
+        }
+        int[] res = new int[nums.length - k + 1]; //target result
+        res[0] = nums[queue.peekFirst()]; //result of the first window
+        int resultIndex = 1;
+        for (int i = k; i < nums.length; i++) {
+            if (queue.peekFirst() + k - 1 < i) { //if the index at the start of the queue has fallen out of the window
+                queue.pollFirst();
+            }
+            while (!queue.isEmpty() && nums[queue.peekLast()] <= nums[i]) { //remove all elements smaller than current element
+                queue.pollLast();
+            }
+            queue.add(i);
+            res[resultIndex++] = nums[queue.peekFirst()]; //index at the head of the queue denotes the result for this window
+        }
+        return res;
+    }
+}
