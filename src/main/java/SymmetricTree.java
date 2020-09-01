44304da83b738785df00cf5a5f54f1c0152bf3c3
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-01 20:00:42 +0530

    Update SymmetricTree with problem statement and simplify if/else

--- src/main/java/SymmetricTree.java
+++ src/main/java/SymmetricTree.java
@@ -2,6 +2,26 @@ import common.TreeNode;
 
 /**
  * https://leetcode.com/problems/symmetric-tree/
+ * <p>
+ * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+ * <p>
+ * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+ * <pre>
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ * </pre>
+ * <p>
+ * But the following [1,2,2,null,3,null,3] is not:
+ * <pre>
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ * </pre>
  */
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
@@ -12,15 +32,11 @@ public class SymmetricTree {
     }
 
     private boolean isSymmetricHelper(TreeNode left, TreeNode right) {
-        if (left == null) {
-            return right == null;
-        } else if (right == null) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
             return false;
         }
-        if (left.val != right.val) {
-            return false;
-        } else {
-            return isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
-        }
+        return left.val == right.val && isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
     }
 }
