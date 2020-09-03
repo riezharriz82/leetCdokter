7c1ed458bc90b7fcdb160186e58cbfd22405576b
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-04 01:37:11 +0530

    Solve easy but tricky find whether a string is composed of repeated substrings by checking all the occurrences of the first character and generating substrings
    Learning -- Initially I was thinking of checking substrings of length 2,3,4... and to see whether the input string length is a multiplier of substring length. If yes multiply the substring and check whether the expected string matches the input string. Then I thought it would be slow, but after solving it and moving on to discuss, realized that that was one of the most upvoted solutions. FML.

---
+++ src/main/java/RepeatedSubstringPattern.java
@@ -0,0 +1,39 @@
+/**
+ * https://leetcode.com/problems/repeated-substring-pattern/
+ * <p>
+ * Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together.
+ * You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+ * <p>
+ * Input: "abab"
+ * Output: True
+ * Explanation: It's the substring "ab" twice.
+ */
+public class RepeatedSubstringPattern {
+    /**
+     * Since the string is repeated, this means that the first character needs to occurs > 1 times in the input
+     * So we get all the occurrences of the first character and try to repeat the substring from the occurrence of that char till the end
+     * This is not the fastest solution but my original solution.
+     */
+    public boolean repeatedSubstringPattern(String s) {
+        int start = 1; //if start from 0, then the entire string is considered
+        while (true) {
+            int indexOf = s.indexOf(s.charAt(0), start++);
+            if (indexOf == -1) {
+                //no remaining occurrences of first char
+                return false;
+            }
+            String candidate = s.substring(indexOf);
+            if (s.length() % candidate.length() != 0) {
+                continue;
+            }
+            int multiplier = s.length() / candidate.length();
+            StringBuilder expected = new StringBuilder();
+            for (int i = 0; i < multiplier; i++) {
+                expected.append(candidate);
+            }
+            if (expected.toString().equals(s)) {
+                return true;
+            }
+        }
+    }
+}
