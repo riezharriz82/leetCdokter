cdf21a92d11787b71178ddcbabcc65a1ec34a6e3
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-12 21:04:27 +0530

    Update approach in finding shortest subarray that needs to be sorted to make the resultant array sorted.
    Learnings -- In problems that requires finding a single subarray that does something, start from left and right and mark the initial boundary. Then check whether that boundary needs to be expanded further.

--- src/main/java/ShortestUnsortedContinuousSubarray.java
+++ src/main/java/ShortestUnsortedContinuousSubarray.java
@@ -13,6 +13,12 @@ import java.util.Arrays;
  * Explanation: You need to sort [6, 4, 8, 10, 9] in ascending order to make the whole array sorted in ascending order.
  */
 public class ShortestUnsortedContinuousSubarray {
+    /**
+     * Approach: Sort the array and then start comparing the sorted array with the input array from left and right.
+     * First index from the left that has a mismatch will provide the left boundary
+     * First index from the right that has a mismatch will provide the right boundary
+     * Time Complexity: nlogn
+     */
     public int findUnsortedSubarray(int[] nums) {
         int[] copy = Arrays.copyOf(nums, nums.length);
         Arrays.sort(copy);
@@ -36,31 +42,36 @@ public class ShortestUnsortedContinuousSubarray {
         return rightIndex - leftIndex + 1;
     }
 
+    /**
+     * Approach: Keep track of the smallest and largest mismatch because they will determine the boundary of the subarray that needs to be sorted.
+     * Reiterate the array, the first number > min will mark the left boundary because the min needs to be placed at that index in order to make the result sorted
+     * Similarly the first number from the right < max will mark the right boundary because the max needs to be placed at that index.
+     */
     public int findUnsortedSubarrayInLinearTime(int[] nums) {
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < nums.length - 1; i++) {
             if (nums[i + 1] < nums[i]) {
-                min = Math.min(min, nums[i + 1]);
+                min = Math.min(min, nums[i + 1]); //keep track of the smallest mismatch
             }
         }
         int max = Integer.MIN_VALUE;
         for (int i = nums.length - 1; i > 0; i--) {
             if (nums[i - 1] > nums[i]) {
-                max = Math.max(max, nums[i - 1]);
+                max = Math.max(max, nums[i - 1]); //largest mismatch
             }
         }
-        if (min == Integer.MAX_VALUE && max == Integer.MIN_VALUE) {
+        if (min == Integer.MAX_VALUE && max == Integer.MIN_VALUE) { //no mismatch
             return 0;
         }
         int l = 0, r = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > min) {
+            if (nums[i] > min) { //min needs to be placed at index i
                 l = i;
                 break;
             }
         }
         for (int j = nums.length - 1; j > 0; j--) {
-            if (nums[j] < max) {
+            if (nums[j] < max) { //max needs to be placed at index j
                 r = j;
                 break;
             }
