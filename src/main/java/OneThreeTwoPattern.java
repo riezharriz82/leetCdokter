1bb3d94609be4fdeacda2dfbab829404a162be1e
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-20 19:41:14 +0530

    Solve medium but hard find 132 pattern in array i.e find three numbers such that a[i] < a[k] < a[j] and i < j < k using monotonic stack
    Learnings -- I solved this problem on my own, took a couple of hours but went through couple of different approaches and then finalized this. The crux of the problem was to fix i and then look for a valid k for each j. a[i] can be the smallest number on the left of a[j] and a[k] can be the largest number on the right smaller than a[j]. Once we find all three, check if they make 132 pattern.

---
+++ src/main/java/OneThreeTwoPattern.java
@@ -0,0 +1,59 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
+public class OneThreeTwoPattern {
+    /**
+     * Approach: This is my original solution, went through a lot of different approaches before I finalized this.
+     * Approaches I discarded, two stacks solution, one for considering smaller elements on the left and one for smaller elements on the right
+     * Keeping the smallest element on the left and largest element on the right for each indices but it won't work as largest element on the right
+     * will not give the candidate required, we need the largest element on the right smaller than current element {10, 50, 9}
+     * we are interested in finding 9 for 10, not 50
+     * <p>
+     * We can find the largest element on the right smaller than current element using decreasing stack.
+     * so if we know the smallest element on the left for each element and the largest element smaller than current element on the right, we can then find if we
+     * have 132 pattern, why only smallest element?
+     * <p>
+     * because consider input {1,8,10,9} -> {8,10,9} and {1,10,9} are valid answers if we just know that smallest element on left of 10 is 1 and the largest element
+     * on the right smaller than 10 is 9, we can find the pattern. Other solutions may exist but we are interested only in finding one pattern.
+     * <p>
+     * {@link LargestRectangleInHistogram} is similar problem. This problem can be done without taking any additional array apart from stack but since
+     * this is my original solution, I find it easy to understand.
+     */
+    public boolean find132pattern(int[] nums) {
+        ArrayDeque<Integer> maxStack = new ArrayDeque<>(); //decreasing stack of indices
+        int[] minValues = new int[nums.length]; //smallest element on the left of ith index
+        int[] maxValues = new int[nums.length]; //largest element on the right smaller than value at current index
+        minValues[0] = Integer.MAX_VALUE;
+        Arrays.fill(maxValues, Integer.MAX_VALUE);
+        int curMin = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < curMin) { //all elements on left are bigger than current element, update the curMin
+                minValues[i] = Integer.MAX_VALUE;
+                curMin = nums[i];
+            } else {
+                //found a smaller element on the left, carry forward the curMin
+                minValues[i] = curMin;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            while (!maxStack.isEmpty() && nums[maxStack.peek()] <= nums[i]) { //we are interested only in finding smaller element on the right
+                maxStack.pop();
+            }
+            if (maxStack.isEmpty()) {
+                //largest element found, do nothing
+            } else {
+                //current element is smaller than the index at the top of the stack, update the maxValues[]
+                //do dry run for {7,3,1,4,8}
+                maxValues[maxStack.peek()] = nums[i];
+            }
+            maxStack.push(i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            //check if minValue[i] < maxValue[i] < nums[i] i.e. 132 pattern
+            if (minValues[i] != Integer.MAX_VALUE && maxValues[i] != Integer.MAX_VALUE && minValues[i] < nums[i] && maxValues[i] < nums[i] && minValues[i] < maxValues[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
