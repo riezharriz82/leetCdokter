e91b2e9cb7fc3dfa939674f780dd004ed4c2a656
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-20 20:24:23 +0530

    Simplify code in SubarraySumEqualsK

--- src/main/java/SubarraySumEqualsK.java
+++ src/main/java/SubarraySumEqualsK.java
@@ -10,42 +10,19 @@ import java.util.HashMap;
  * Output: 2
  */
 public class SubarraySumEqualsK {
-    public int subarraySumWithoutHashMap(int[] nums, int k) {
-        int min = nums[0], max = nums[0], curSum = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            curSum += nums[i];
-            min = Math.min(min, curSum);
-            max = Math.max(max, curSum);
-        }
-        int[] reverseLookUp = new int[max - min + 1]; // if the min is 1 and the max is 3, there can be at max 3 elements
-        curSum = 0;
-        int res = 0;
-        for (int num : nums) {
-            curSum += num;
-            if (curSum == k) {
-                res++;
-            }
-            if (curSum - k >= min && curSum - k <= max) {
-                res += reverseLookUp[curSum - k - min];
-            }
-            reverseLookUp[curSum - min]++;
-        }
-        return res;
-    }
-
-
+    /**
+     * Approach: Standard subarray problem dealing with prefix sum. Need a hashmap to store all the prefix sum.
+     */
     public int subarraySum(int[] nums, int k) {
         int res = 0, curSum = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>(); //key as prefix sum and value as number of occurrences
+        map.put(0, 1); //to handle subarray sum starting from 0
         for (int num : nums) {
             curSum += num;
-            if (curSum == k) {
-                res++;
-            }
             if (map.containsKey(curSum - k)) {
                 res += map.get(curSum - k);
             }
-            map.put(curSum, map.getOrDefault(curSum, 0) + 1);
+            map.put(curSum, map.getOrDefault(curSum, 0) + 1); //there can be multiple subarrays with same sum
         }
         return res;
     }
