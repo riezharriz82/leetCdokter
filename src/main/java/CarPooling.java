49e655ce1592bca6436d5fbbcd61f37ee3f029e4
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-21 23:23:09 +0530

    Solve medium find max capacity required to car pool given start, end time and no of people hopping in by greedy approach. Keep track of people hopping in and out at each time.
    Learnings -- It's very critical to understand that if you copy the solution of other users without understanding why they used a specific DS, you won't be able to solve similar questions in the future. This question is similar to MeetingsRoom2

---
+++ src/main/java/CarPooling.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/car-pooling/
+ * <p>
+ * You are driving a vehicle that has capacity empty seats initially available for passengers.  The vehicle only drives east (ie. it cannot turn around and drive west.)
+ * <p>
+ * Given a list of trips, trip[i] = [num_passengers, start_location, end_location] contains information about the i-th trip:
+ * the number of passengers that must be picked up, and the locations to pick them up and drop them off.
+ * The locations are given as the number of kilometers due east from your vehicle's initial location.
+ * <p>
+ * Return true if and only if it is possible to pick up and drop off all passengers for all the given trips.
+ * <p>
+ * Input: trips = [[2,1,5],[3,3,7]], capacity = 4
+ * Output: false
+ */
+public class CarPooling {
+
+    /**
+     * {@link alternate.MeetingsRoom2} is similar problem
+     * Approach: Greedy, keep track of the persons hopping in and dropping out at any moment, current capacity should not exceed max capacity
+     * <p>
+     * Solved it on my own this time, it's very critical to understand that if you copy the solution of other users without understanding why they used
+     * a specific DS, you won't be able to solve similar questions in the future.
+     * <p>
+     * I tried to solve MeetingsRoom2 after this problem, couldn't solve it without using counting sort as used in this approach because I could not
+     * understand why the author used PriorityQueue instead of just comparing it with the previous interval
+     */
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] count = new int[1001]; //max distance of the trip is 1000
+        for (int[] trip : trips) {
+            count[trip[1]] += trip[0]; //person hopping in
+            count[trip[2]] -= trip[0]; //person dropping out
+        }
+        int currentCapacity = 0;
+        for (int cnt : count) {
+            currentCapacity += cnt;
+            if (currentCapacity > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Previous solution used a array to keep track of people coming in and out but it's not scalable for long sized input.
+     * We can use map to do the counting.
+     */
+    public boolean carPoolingGeneral(int[][] trips, int capacity) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int[] trip : trips) {
+            map.put(trip[1], map.getOrDefault(trip[1], 0) + trip[0]);
+            map.put(trip[2], map.getOrDefault(trip[2], 0) - trip[0]);
+        }
+        int currentCapacity = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            currentCapacity += entry.getValue();
+            if (currentCapacity > capacity) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/alternate/MeetingsRoom2.java
+++ src/main/java/alternate/MeetingsRoom2.java
@@ -15,18 +15,26 @@ import java.util.PriorityQueue;
  * return 2.
  */
 public class MeetingsRoom2 {
+
+    /**
+     * Approach: It's critical to understand why we chose priority queue instead of simply comparing the start time of current interval with
+     * end time of previous interval e.g {2,8},{5,15},{10,20}
+     * we need to remove all intervals ending before the start of current interval, we can do that by priority queue.
+     * <p>
+     * {@see CarPooling} for solving it in other way
+     */
     public int minMeetingRooms(Interval[] intervals) {
         Arrays.sort(intervals, Comparator.comparingInt(a -> a.start));
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        minHeap.add(intervals[0].end);
+        minHeap.add(intervals[0].end); //we are only interested in end time
         for (int i = 1; i < intervals.length; ++i) {
             if (minHeap.peek() <= intervals[i].start) {
                 //if the current interval starts after any finished meeting, remove the finished meeting
+                //any part is important, that's why we used priority queue, not stack
                 minHeap.poll();
             }
             minHeap.add(intervals[i].end);
         }
-
         return minHeap.size();
     }
 }
