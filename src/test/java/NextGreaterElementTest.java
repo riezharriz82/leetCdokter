f6a2d7338f2213f9190850a22002a0007d5cfb99
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-26 22:36:29 +0530

    Fix broken tests

--- src/test/java/NextGreaterElementTest.java
+++ src/test/java/NextGreaterElementTest.java
@@ -5,6 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 public class NextGreaterElementTest {
     @Test
     public void test() {
-        assertArrayEquals(new int[]{1, 1, 4, 2, 1, 1, 0, 0}, new NextGreaterElement().dailyTemperaturesUsingCustomStack(new int[]{73, 74, 75, 71, 69, 72, 76, 73}));
+        assertArrayEquals(new int[]{1, 1, 4, 2, 1, 1, 0, 0}, new NextGreaterElement().dailyTemperatures(new int[]{73, 74, 75, 71, 69, 72, 76, 73}));
     }
 }
--- src/test/java/SubarraySumsEqualKTest.java
+++ src/test/java/SubarraySumsEqualKTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SubarraySumsEqualKTest {
     @Test
     public void test() {
-        assertEquals(2, new SubarraySumEqualsK().subarraySumWithoutHashMap(new int[]{1, 1, 1}, 2));
-        assertEquals(4, new SubarraySumEqualsK().subarraySumWithoutHashMap(new int[]{0, 0, 0, 3}, 3));
+        assertEquals(2, new SubarraySumEqualsK().subarraySum(new int[]{1, 1, 1}, 2));
+        assertEquals(4, new SubarraySumEqualsK().subarraySum(new int[]{0, 0, 0, 3}, 3));
     }
 }
