570609b7827c870972cf5d8221de78560c5e9c30
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-29 00:22:20 +0530

    Solve medium find minimum size subarray whose sum >= target sum by sliding window. Keep increasing the window until curSum >= targetSum. Once such window is found, keep shortening the window to find a smaller valid window.
    Learnings -- Utilized the learnings from previous solved problem MinimumWindowSubstring

---
+++ src/main/java/MinimumSizeSubarraySum.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode.com/problems/minimum-size-subarray-sum/
+ * <p>
+ * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s.
+ * If there isn't one, return 0 instead.
+ * <p>
+ * Input: s = 7, nums = [2,3,1,2,4,3]
+ * Output: 2
+ * Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+ */
+public class MinimumSizeSubarraySum {
+    /**
+     * Approach: Sliding window, keep increasing the window until the current sum of the window >= target
+     * Once such window is reached, keep decreasing the window until the window becomes invalid.
+     * Keep track of the min window size.
+     * <p>
+     * {@link MinimumWindowSubstring} for related problem
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int begin = 0, end = 0, minLength = Integer.MAX_VALUE;
+        int curSum = 0;
+        while (end < nums.length) {
+            curSum += nums[end];
+            while (curSum >= target) { //once a valid window is reached, keep shortening the window to find a smaller valid subarray
+                if (end - begin + 1 < minLength) {
+                    minLength = end - begin + 1;
+                }
+                curSum -= nums[begin];
+                begin++;
+            }
+            end++;
+        }
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+}
