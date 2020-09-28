2f43690da82fbbdb3ecfec3a0738c5dde3eb9d7c
snehasishiiita <rit2011091@iiita.ac.in>
2020-09-29 02:03:37 +0530

    Solve medium find count of subarrays whose product is < k by sliding window approach.
    Learnings -- Counting no of valid subarrays via sliding window was tricky, use (end-start+1)
    Keep increasing the window until current product < k. If not, decrement the window from the start

---
+++ src/main/java/SubarrayProductLessThanK.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/subarray-product-less-than-k/
+ * <p>
+ * Your are given an array of positive integers nums.
+ * <p>
+ * Count and print the number of (contiguous) subarrays where the product of all the elements in the subarray is less than k.
+ * <p>
+ * Input: nums = [10, 5, 2, 6], k = 100
+ * Output: 8
+ * Explanation: The 8 subarrays that have product less than 100 are: [10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6].
+ * Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+ */
+public class SubarrayProductLessThanK {
+    /**
+     * Approach: Sliding window, keep increasing the window until current product < k. If not, decrement the window from the start
+     * Only trick is how to count the number of subarrays ? if it would have been length, it would be trivial.
+     * <p>
+     * To count the no of subarrays after increasing or decreasing the window use (end-start+1)
+     */
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1) {
+            return 0;
+        }
+        int begin = 0, end = 0, res = 0;
+        long curProduct = 1;
+        while (end < nums.length) {
+            curProduct *= nums[end];
+            while (curProduct >= k) {
+                curProduct /= nums[begin];
+                begin++;
+            }
+            res += (end - begin + 1);
+            end++;
+        }
+        return res;
+    }
+}
