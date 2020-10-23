27e015c37f6aaa1be5a5b9119c4a00f0340cc719
snehasishiiita <rit2011091@iiita.ac.in>
2020-10-23 20:02:10 +0530

    Add greedy approach of solving hard split array into m subarrays such that largest sum is maximized. Need to perform binary search on the largest sum possible for single sized subarray and largest sum possible if we choose the entire array. Target subarray sum would be somewhere in between. If our chosen candidate sum results in split < target split, we have to decrease the candidate sum else increase the candidate sum

--- src/main/java/SplitArrayLargestSum.java
+++ src/main/java/SplitArrayLargestSum.java
@@ -21,8 +21,6 @@ public class SplitArrayLargestSum {
      * Compare the maximum of left and right, lets name it maxSum.
      * As we make multiple cuts at all the places, keep track of the smallest value of maxSum.
      * Smallest value of maxSum would be the result.
-     * <p>
-     * TODO It can be optimized by using binary search
      */
     public int splitArray(int[] nums, int m) {
         int[][] memoization = new int[nums.length + 1][m + 1];
@@ -50,5 +48,53 @@ public class SplitArrayLargestSum {
             minSum = Math.min(minSum, Math.max(curSum, remaining)); //keep track of the smallest value of maximum of left and right subarray sum//
         }
         return memoization[index][splits] = minSum;
+    }
+
+    /**
+     * Approach: Greedy + Binary Search
+     * if the target subarray is of size = 1, what can be the maximum subarray sum, it will be the max(array elements)
+     * if the target subarray is of size = n, what can be the maximum subarray sum, it wil be sum of all array elements
+     * <p>
+     * So we do binary search between those two bounds, and check for each mid, how many splits can be performed on the input array
+     * Consider a target sum that is the largest possible subarray sum and then try to split the array and see how many splits can be performed
+     * e.g. {1,2,3,4,5,6,7} if target sum is 10, splits are {1,2,3,4} {5} {6} {7} total 4 splits
+     * if we have to split the array into 3 splits, we can increase our target sum because greater the target sum lesser the splits
+     * similarly if required splits is 5, we have to reduce target sum
+     */
+    public int splitArrayGreedy(int[] nums, int m) {
+        int sum = 0, maxValue = Integer.MIN_VALUE;
+        for (int num : nums) {
+            sum += num;
+            maxValue = Math.max(maxValue, num);
+        }
+        if (m == 1) {
+            return sum;
+        }
+        int low = maxValue, high = sum;
+        int ans = 0; //erichtos' binary search template
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (splitArrayPartitions(nums, mid) < m) { //target is high, need to reduce target sum so that we can get more splits
+                high = mid - 1;
+                ans = mid; //erichto's template of storing result in ans, avoids pesky conditions
+            } else if (splitArrayPartitions(nums, mid) >= m) {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    //return how many splits are required to divide array given target subarray sum i.e. the max subarray sum
+    private int splitArrayPartitions(int[] nums, int target) {
+        int curSplits = 0;
+        int curSum = 0;
+        for (int num : nums) {
+            curSum += num;
+            if (curSum > target) { //need to split it
+                curSplits++;
+                curSum = num; //current number can't be part of previous split, needs to be part of new split
+            }
+        }
+        return curSplits;
     }
 }
