bccb51580a4783cfccd3a4320e8b762b7791c723
snehasishiiita <rit2011091@iiita.ac.in>
2020-10-24 17:44:12 +0530

    Update example input and and gotcha in CourseSchedule2

--- src/main/java/CourseSchedule2.java
+++ src/main/java/CourseSchedule2.java
@@ -12,6 +12,16 @@ import java.util.List;
  * Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
  * <p>
  * There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+ * <p>
+ * Input: numCourses = 2, prerequisites = [[1,0]]
+ * Output: [0,1]
+ * Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So the correct course order is [0,1].
+ * <p>
+ * Input: numCourses = 4, prerequisites = [[1,0],[2,0],[3,1],[3,2]]
+ * Output: [0,2,1,3]
+ * Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both courses 1 and 2.
+ * Both courses 1 and 2 should be taken after you finished course 0.
+ * So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3].
  */
 public class CourseSchedule2 {
     //topological sort can also be done by bfs ie. kahn algorithm
@@ -58,7 +68,7 @@ public class CourseSchedule2 {
             graph.add(new ArrayList<>());
         }
         for (int i = 0; i < prerequisites.length; i++) {
-            graph.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            graph.get(prerequisites[i][1]).add(prerequisites[i][0]); //ordering is a bit important, otherwise need to reverse the stack
         }
         return graph;
     }
