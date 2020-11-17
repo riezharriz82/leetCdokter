a2d012d446533318e2bb31525e6cab6d1cfd69d9
snehasishiiita <rit2011091@iiita.ac.in>
2020-11-17 22:48:38 +0530

    Add problem statement in 132 pattern -- still surprised to see that I was able to solve that question in linear time on my own :D

--- src/main/java/OneThreeTwoPattern.java
+++ src/main/java/OneThreeTwoPattern.java
@@ -1,6 +1,27 @@
 import java.util.ArrayDeque;
 import java.util.Arrays;
 
+/**
+ * https://leetcode.com/problems/132-pattern/
+ * <p>
+ * Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+ * <p>
+ * Return true if there is a 132 pattern in nums, otherwise, return false.
+ * <p>
+ * Follow up: The O(n^2) is trivial, could you come up with the O(n logn) or the O(n) solution?
+ * <p>
+ * Input: nums = [1,2,3,4]
+ * Output: false
+ * Explanation: There is no 132 pattern in the sequence.
+ * <p>
+ * Input: nums = [3,1,4,2]
+ * Output: true
+ * Explanation: There is a 132 pattern in the sequence: [1, 4, 2].
+ * <p>
+ * Input: nums = [-1,3,2,0]
+ * Output: true
+ * Explanation: There are three 132 patterns in the sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+ */
 public class OneThreeTwoPattern {
     /**
      * Approach: This is my original solution, went through a lot of different approaches before I finalized this.
