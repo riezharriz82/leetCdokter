ab7c9cd872f3e1230f9f4a7125aaaeb3bb7626b7
snehasishiiita <rit2011091@iiita.ac.in>
2020-12-08 20:58:43 +0530

    Add missing sample input to the problem statement

--- src/main/java/PossibleBipartition.java
+++ src/main/java/PossibleBipartition.java
@@ -12,6 +12,7 @@ import java.util.List;
  * <p>
  * Return true if and only if it is possible to split everyone into two groups in this way.
  * <p>
+ * Input: N = 4, dislikes = [[1,2],[1,3],[2,4]]
  * Output: true
  * Explanation: group1 [1,4], group2 [2,3]
  * <p>
