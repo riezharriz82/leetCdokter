30ced615040d5ec88f4840e654cef7a84ef7191a
snehasishiiita <rit2011091@iiita.ac.in>
2020-12-22 01:41:16 +0530

    Simplify code by removing complicated bottom up code and add comment in existing top down code

--- src/main/java/SumOfNodesWithEvenGrandParents.java
+++ src/main/java/SumOfNodesWithEvenGrandParents.java
@@ -11,20 +11,20 @@ import common.TreeNode;
  * Output: 18
  */
 public class SumOfNodesWithEvenGrandParents {
-    int result = 0;
-
     public int sumEvenGrandparentSimplified(TreeNode root) {
         return DFS(root, 1, 1); //init it will any odd value
     }
 
-    //Approach: Let nodes be aware of their grandparent so they can add the current node value to the result
-    // Update parent and grandparent for next iteration
-    // Top Down approach
+    /**
+     * Approach: Let nodes be aware of their grandparent so they can add the current node value to the result
+     * Update parent and grandparent for next iteration
+     * Top Down approach
+     */
     private int DFS(TreeNode root, int parent, int grandParent) {
         if (root == null) {
             return 0;
         } else {
-            int left = DFS(root.left, root.val, parent);
+            int left = DFS(root.left, root.val, parent); //make root as the new parent of the children and current parent as the new grandparent
             int right = DFS(root.right, root.val, parent);
             if (grandParent % 2 == 0) {
                 return left + right + root.val;
@@ -32,48 +32,5 @@ public class SumOfNodesWithEvenGrandParents {
                 return left + right;
             }
         }
-    }
-
-    public int sumEvenGrandparent(TreeNode root) {
-        DFS(root);
-        return result;
-    }
-
-    //Bottom up approach
-    //Each node return their child and grandchild sum to the parent (Special case for leaf nodes)
-    //Parent mark the child sum of their child as their grandchild sum and update the child sum too
-    private Result DFS(TreeNode root) {
-        if (root == null) {
-            return new Result();
-        } else {
-            if (root.left == null && root.right == null) {
-                Result result = new Result();
-                result.leaf = root.val;
-                return result;
-            } else {
-                Result left = DFS(root.left);
-                Result right = DFS(root.right);
-                int childSum = 0;
-                childSum += left.leaf;
-                childSum += right.leaf;
-                childSum += left.child;
-                childSum += right.child;
-                if (root.val % 2 == 0) {
-                    result += left.grandChild;
-                    result += right.grandChild;
-                }
-                Result result = new Result();
-                //tricky: Update the grandchild and child as we return to the parent
-                result.grandChild = childSum;
-                result.child = root.val;
-                return result;
-            }
-        }
-    }
-
-    private static class Result {
-        public int child;
-        public int grandChild;
-        public int leaf;
     }
 }
