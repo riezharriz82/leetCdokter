3d2a37a30c5a784bb37e70d47eb435d0084adbbd
snehasishiiita <rit2011091@iiita.ac.in>
2020-12-27 18:35:28 +0530

    Add clarification around why sorting is required because we need to pick a set of valid envelopes, rather than a subsequence

--- src/main/java/RussianDollEnvelopes.java
+++ src/main/java/RussianDollEnvelopes.java
@@ -24,7 +24,8 @@ public class RussianDollEnvelopes {
         if (envelopes.length <= 1) {
             return envelopes.length;
         }
-        Arrays.sort(envelopes, (o1, o2) -> { //increasing sort
+        //increasing sort because problem statement states we can pick any set of envelopes, rather than picking only subsequences
+        Arrays.sort(envelopes, (o1, o2) -> {
             if (o1[0] == o2[0]) {
                 return Integer.compare(o1[1], o2[1]);
             } else {
