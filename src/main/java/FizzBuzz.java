7ffe8e6d01254c38381b9575848fcb4bf23521c1
snehasishiiita <rit2011091@iiita.ac.in>
2021-03-29 23:01:06 +0530

    Add problem statement and approach for FizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,8 +3,39 @@ import java.util.List;
 
 /**
  * https://leetcode.com/problems/fizz-buzz/
+ * <p>
+ * Write a program that outputs the string representation of numbers from 1 to n.
+ * <p>
+ * But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+ * For numbers which are multiples of both three and five output “FizzBuzz”.
+ * <pre>
+ * n = 15,
+ * Return:
+ * [
+ *     "1",
+ *     "2",
+ *     "Fizz",
+ *     "4",
+ *     "Buzz",
+ *     "Fizz",
+ *     "7",
+ *     "8",
+ *     "Fizz",
+ *     "Buzz",
+ *     "11",
+ *     "Fizz",
+ *     "13",
+ *     "14",
+ *     "FizzBuzz"
+ * ]
+ * </pre>
  */
 public class FizzBuzz {
+    /**
+     * Approach: Ad-hoc, make sure to check for divisibility by 15 first.
+     * <p>
+     * {@link concurrency.FizzBuzzMultithreaded}
+     */
     public List<String> fizzBuzz(int n) {
         ArrayList<String> result = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
